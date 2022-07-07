191ca8e78d3be8f5b087266d96b919c3f20aeaa5
benku101 <99992295+benku101@users.noreply.github.com>
2022-07-06 19:57:19 -0500

    change words

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ const App = () => {
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header"> Buildspace Emoji Store!</p>
+          <p className="sub-text">The only emoji store that accepts random coins</p>
         </header>
 
         <main>
